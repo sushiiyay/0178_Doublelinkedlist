386740b1a633606e08d5903d85ec631c51a4c5c4
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-01 15:44:31 +0700

    Menambahkan node

--- Doublelinklist.cpp
+++ Doublelinklist.cpp
@@ -83,4 +83,17 @@ public:
     }
 
     void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+    }
 }
