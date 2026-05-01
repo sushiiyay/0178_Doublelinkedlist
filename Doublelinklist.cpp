f77565b65663dba99250d4ef36f0cae0aa3ac354
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-01 15:22:57 +0700

    Step 1

--- Doublelinklist.cpp
+++ Doublelinklist.cpp
@@ -27,5 +27,10 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        //
     }
 }
