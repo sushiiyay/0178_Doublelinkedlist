7e1b22133314a99b84c9c9257e19ca05758a95c4
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-01 15:23:43 +0700

    Step 2

--- Doublelinklist.cpp
+++ Doublelinklist.cpp
@@ -31,6 +31,7 @@ public:
         // Step 1: Allocate memory for new node
         Node *newNode = new Node();
 
-        //
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
     }
 }
