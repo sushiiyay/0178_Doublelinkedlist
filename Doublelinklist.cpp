f9ee20adf737aa61e7e1880c95b8ac48b171c7be
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-01 15:29:59 +0700

    Step 5

--- Doublelinklist.cpp
+++ Doublelinklist.cpp
@@ -43,6 +43,11 @@ public:
                 return;
             }
             // Step 4: newNode.next = START
+            newNode->next = START;
+
+            // Step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
         }
     }
 }
